193b1aa171d9586debe33211138506e1b51ef2bc
PerchunPak <git@perchun.it>
2023-07-21 16:44:10 +0200

    Fix cursed formatting

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -3,9 +3,9 @@ import { register, init } from 'svelte-i18n';
 
 const defaultLocale = 'en';
 
-register('en', () => import('./lang/en.json')),
-	register('ru', () => import('./lang/ru.json')),
-	register('uk', () => import('./lang/uk.json'));
+register('en', () => import('./lang/en.json'));
+register('ru', () => import('./lang/ru.json'));
+register('uk', () => import('./lang/uk.json'));
 
 init({
 	fallbackLocale: defaultLocale,
