a5ddf77b84c571b52fe532c5cb53c9ab29a3dc30
PerchunPak <git@perchun.it>
2023-07-22 20:30:11 +0200

    Generate more nodes with quite random uptime

--- src/lib/nodes.ts
+++ src/lib/nodes.ts
@@ -1,22 +1,29 @@
-export const nodes =
-	[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0].map((i) => ({
-		name: 'rutherford',
-		isDown: false,
-		uptime: 80.21 + (i + 10),
-		fuckUps: [
-			{
-				date: 1689839351,
-				duration: 103
-			},
-			{
-				date: 1689871751,
-				duration: 124
-			},
-			{
-				date: 1689911351,
-				duration: 32
-			}
-		],
-		monitoringSince: 1689738551
-	}))
-;
+function generateRandomUptimeAfterDot() {
+	const min = Math.ceil(0);
+	const max = Math.floor(99);
+	return Math.floor(Math.random() * (max - min + 1) + min) / 100;
+}
+
+export const nodes = Array.from(
+	{ length: 12 },
+	(_, i) => i + 89 + generateRandomUptimeAfterDot()
+).map((i) => ({
+	name: 'rutherford',
+	isDown: false,
+	uptime: i,
+	fuckUps: [
+		{
+			date: 1689839351,
+			duration: 103
+		},
+		{
+			date: 1689871751,
+			duration: 124
+		},
+		{
+			date: 1689911351,
+			duration: 32
+		}
+	],
+	monitoringSince: 1689738551
+}));
