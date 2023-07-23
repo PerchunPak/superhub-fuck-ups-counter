04567d7ac1fc8687c4568a5b9c20a54b37e2738b
PerchunPak <git@perchun.it>
2023-07-23 19:37:19 +0200

    Add i18n for sorting

--- src/routes/components/SelectSortBy.svelte
+++ src/routes/components/SelectSortBy.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
 	import { sortNodesBy } from '$lib/stores';
 </script>
 
 <div class="form-control w-full">
 	<label class="label">
-		<span class="label-text">Sort by</span>
+		<span class="label-text">{$_("sorting.Sort by")}</span>
 	</label>
 	<select class="select select-bordered" bind:value={$sortNodesBy}>
-		<option selected>Uptime percent</option>
-		<option>Name</option>
-		<option>Count of fuck-ups</option>
-		<option>Total downtime</option>
-		<option>Last downtime</option>
-		<option>First noticed</option>
+		<option selected>{$_("sorting.Uptime percent")}</option>
+		<option>{$_("sorting.Name")}</option>
+		<option>{$_("sorting.Count of fuck-ups")}</option>
+		<option>{$_("sorting.Total downtime")}</option>
+		<option>{$_("sorting.Last downtime")}</option>
+		<option>{$_("sorting.First noticed")}</option>
 	</select>
 </div>
