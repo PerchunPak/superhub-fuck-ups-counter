34d719767de30811b26dccc4ca509554d7b99173
PerchunPak <git@perchun.it>
2023-07-24 00:28:11 +0200

    Format SelectSortBy.svelte

--- src/routes/components/SelectSortBy.svelte
+++ src/routes/components/SelectSortBy.svelte
@@ -5,14 +5,14 @@
 
 <div class="form-control w-full">
 	<label class="label">
-		<span class="label-text">{$_("sorting.Sort by")}</span>
+		<span class="label-text">{$_('sorting.Sort by')}</span>
 	</label>
 	<select class="select select-bordered" bind:value={$sortNodesBy}>
-		<option selected value="uptime-percent">{$_("sorting.Uptime percent")}</option>
-		<option value="name">{$_("sorting.Name")}</option>
-		<option value="fuck-ups-count">{$_("sorting.Count of fuck-ups")}</option>
-		<option value="total-downtime">{$_("sorting.Total downtime")}</option>
-		<option value="last-downtime">{$_("sorting.Last downtime")}</option>
-		<option value="first-noticed">{$_("sorting.First noticed")}</option>
+		<option selected value="uptime-percent">{$_('sorting.Uptime percent')}</option>
+		<option value="name">{$_('sorting.Name')}</option>
+		<option value="fuck-ups-count">{$_('sorting.Count of fuck-ups')}</option>
+		<option value="total-downtime">{$_('sorting.Total downtime')}</option>
+		<option value="last-downtime">{$_('sorting.Last downtime')}</option>
+		<option value="first-noticed">{$_('sorting.First noticed')}</option>
 	</select>
 </div>
